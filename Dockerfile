490627522bc67b01d79adaaed011d9d732908b8a
Guo Xiayu <xiayu.guo@shopee.com>
2022-05-20 10:30:02 +0800

    update dockerfile

--- Dockerfile
+++ Dockerfile
@@ -1,10 +1,14 @@
 FROM python:3-alpine
 
 WORKDIR /app
-COPY requirements.txt ./
-COPY genshincheckinhelper ./genshincheckinhelper
-COPY genshinhelper ./genshinhelper
+
+RUN apk update && apk add git
+RUN git clone --depth 1 https://gitlab.com/gxy12345/genshin-checkin-helper.git
+
+WORKDIR genshin-checkin-helper
 
 RUN pip install --no-cache-dir -r requirements.txt
+COPY entrypoint.sh docker-entrypoint.sh
+RUN chmod +x docker-entrypoint.sh
 
-CMD [ "python3", "./genshincheckinhelper/main.py" ]
+ENTRYPOINT ["./docker-entrypoint.sh"]
--- entrypoint.sh
+++ entrypoint.sh
@@ -5,10 +5,15 @@ echo -e "\n ======== \n ${Info} ${GreenBG} 拉取最新项目 ${Font} \n =======
 git pull
 
 echo -e "\n ======== \n ${Info} ${GreenBG} 更新运行依赖 ${Font} \n ======== \n"
-pip3 install --no-cache-dir -r requirements.txt
+pip3 install -r requirements.txt
 
-echo -e "\n ======== \n ${Info} ${GreenBG} 读取config文件 ${Font} \n ======== \n"
-cp /app/genshincheckinhelper/config.json ./genshincheckinhelper/config/config.json
+echo -e "\n ======== \n ${Info} ${GreenBG} 读取旧config文件 ${Font} \n ======== \n"
+if [ -f "/app/genshincheckinhelper/config/config.json" ]; then
+  echo -e "发现旧路径下的配置文件，进行复制..."
+  cp /app/genshincheckinhelper/config/config.json ./genshincheckinhelper/config/config.json
+else
+  echo -e "旧版配置目录未发现配置文件，使用新版配置路径"
+fi
 
-echo -e "\n ======== \n ${Info} ${GreenBG} 启动小助手 ${Font} \n ======== \n"
+echo -e "\n ======== \n ${Info} ${GreenBG} 启动原神小助手 ${Font} \n ======== \n"
 python3 ./genshincheckinhelper/main.py
